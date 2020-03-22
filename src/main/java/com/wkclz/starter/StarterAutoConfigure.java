538758a01998f42ec85ddd4c4187610052a5e8dd
wkclz <wkclz@qq.com>
2020-03-22 13:42:18 +0800

    去除sdk

--- src/main/java/com/wkclz/starter/StarterAutoConfigure.java
+++ src/main/java/com/wkclz/starter/StarterAutoConfigure.java
@@ -1,15 +1,12 @@
 package com.wkclz.starter;
 
 
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients(value = {"com.wkclz.sdk.feign"})
 @ComponentScan(basePackages = {
     "com.wkclz.starter",
-    "com.wkclz.sdk",
     "com.wkclz.core",
 })
 public class StarterAutoConfigure {
